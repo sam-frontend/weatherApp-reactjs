cc9552392982233d3309479b8bdf921fef5345dd
Sam ugonna <sam_ug@yahoo.com>
2024-08-16 09:37:46 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "../src/Components/App/App";
+<link
+  href="https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap"
+  rel="stylesheet"
+/>;
+ReactDOM.render(<App />, document.getElementById("root"));
